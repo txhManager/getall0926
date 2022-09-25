9ebfbde75857db1659413cc42a34b163ed11a324
txh <2321814570@qq.com>
2022-09-25 14:46:36 +0800

    项目经理开发完项目提交

---
+++ git_001_javase/src/main/java/com/bjpowernode/Hello.java
@@ -0,0 +1,7 @@
+package com.bjpowernode;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("这是javase的内容。。。。。。。");
+    }
+}
---
+++ git_002_javaweb/src/main/java/com.bjpowernode/Manager.java
@@ -0,0 +1,7 @@
+package com.bjpowernode;
+
+public class Manager {
+    public static void main(String[] args) {
+        System.out.println("这是Javaweb的内容。。。。。");
+    }
+}
